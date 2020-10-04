4bae770775e3dd610f0e6db7fa8f8e8ca7d49d43
Atila Lima <atilamanager@gmail.com>
2020-10-05 01:32:03 +0900

    setting module controllers on JS file

---
+++ app.js
@@ -0,0 +1,21 @@
+//  BUDGET CONTROLLER
+let budgetController = (function() {
+    //CODE
+})();
+
+//  UI CONTROLLER
+let UIControler = (function() {
+//  code
+})();
+
+
+//  GLOBAL APP CONTROLLER
+let controller = (function(budgetControl, UIControl) {
+    document.getElementsByClassName('add__btn').addEventListener('click', () {
+        // 1. GET THE FIELD INPUT DATA
+        // 2. ADD THE ITEM TO THE BUDGET CONTROLLER
+        // 3. ADD ITEM TO THE UI
+        // 4. CALCULATE THE BUDGET
+        // 5. DISPLAY THE BUDGET IN THE UI
+    })
+})(budgetController, UIControler);
