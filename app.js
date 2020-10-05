86861413080e2eb441937aedb75c0939667db739
Atila Lima <atilamanager@gmail.com>
2020-10-05 14:06:02 +0900

    adding background picture and some DRY design pattern methods

--- app.js
+++ app.js
@@ -4,14 +4,30 @@ let budgetController = (function() {
 })();
 
 //  UI CONTROLLER
-let UIControler = (function() {
+let UIController = (function() {
+
+/* Object to save shortcuts to the DOM classes into variables
+Making it easier to dynamically change and work around them */
+
+    let DOMVariables = {
+        typeInput: '.add__type',
+        descriptionInput: '.add__description',
+        valueInput: '.add__value',
+        buttonInput: '.add__btn'
+    };
+    
     return {
         getInput: () => {
             return {
-            type: document.querySelector('.add__type').value,
-            description: document.querySelector('.add__description').value,
-            value: document.querySelector('.add__value')
-            }
+            type: document.querySelector(DOMVariables.typeInput).value,
+            description: document.querySelector(DOMVariables.descriptionInput).value,
+            value: document.querySelector(DOMVariables.valueInput).value
+            };
+        },
+        /*Create an object with a function that turns the DOMVariables object
+        accessible from within other closures in the code */
+        getDOMVariables: () => {
+            return DOMVariables;
         }
     }
 })();
@@ -20,16 +36,20 @@ let UIControler = (function() {
 //  GLOBAL APP CONTROLLER
 let controller = (function(budgetControl, UIControl) {
 
+    //Create a variable that will evoke the method that makes the DOMVariables accessible
+    let DOM = UIControl.getDOMVariables();
+
     let addItemControl = () => {
          // 1. GET THE FIELD INPUT DATA
          let input = UIControl.getInput();
+         console.log(input);
         // 2. ADD THE ITEM TO THE BUDGET CONTROLLER
         // 3. ADD ITEM TO THE UI
         // 4. CALCULATE THE BUDGET
         // 5. DISPLAY THE BUDGET IN THE UI
     }
 
-    document.getElementsByClassName('add__btn').addEventListener('click', addItemControl);
+    document.querySelector(DOM.buttonInput).addEventListener('click', addItemControl);
 
     document.addEventListener('keypress', addItemControl);
-})(budgetController, UIControler);
+})(budgetController, UIController);
