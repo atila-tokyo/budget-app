8588925333f9991e2d243221782e0817d2bdb0c6
Atila Lima <atilamanager@gmail.com>
2020-10-05 13:31:59 +0900

    Adding method to get input and turn it into an object

--- app.js
+++ app.js
@@ -5,17 +5,31 @@ let budgetController = (function() {
 
 //  UI CONTROLLER
 let UIControler = (function() {
-//  code
+    return {
+        getInput: () => {
+            return {
+            type: document.querySelector('.add__type').value,
+            description: document.querySelector('.add__description').value,
+            value: document.querySelector('.add__value')
+            }
+        }
+    }
 })();
 
 
 //  GLOBAL APP CONTROLLER
 let controller = (function(budgetControl, UIControl) {
-    document.getElementsByClassName('add__btn').addEventListener('click', () {
-        // 1. GET THE FIELD INPUT DATA
+
+    let addItemControl = () => {
+         // 1. GET THE FIELD INPUT DATA
+         let input = UIControl.getInput();
         // 2. ADD THE ITEM TO THE BUDGET CONTROLLER
         // 3. ADD ITEM TO THE UI
         // 4. CALCULATE THE BUDGET
         // 5. DISPLAY THE BUDGET IN THE UI
-    })
+    }
+
+    document.getElementsByClassName('add__btn').addEventListener('click', addItemControl);
+
+    document.addEventListener('keypress', addItemControl);
 })(budgetController, UIControler);
